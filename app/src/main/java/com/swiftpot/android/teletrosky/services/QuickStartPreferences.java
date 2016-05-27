60a455e032cd6163d73c8a45063cc2a94fa680ed
AcePRbk[Rodney] <rodney@smsgh.com>
2016-05-27 17:33:14 +0000

    added config for google play service,yet to add to manifest

---
+++ app/src/main/java/com/swiftpot/android/teletrosky/services/QuickStartPreferences.java
@@ -0,0 +1,16 @@
+package com.swiftpot.android.teletrosky.services;/**
+ * Created by Rodney on 31-Mar-16.
+ */
+
+/**
+ * Created by Ace Programmer Rbk<rodney@swiftpot.com> on 31-Mar-16
+ * 3:39 PM
+ */
+public class QuickStartPreferences {
+
+
+        public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+        public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+
+}
