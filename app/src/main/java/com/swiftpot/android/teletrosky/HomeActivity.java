738aa5ed01ae0e11c30bcf75cc8d6e4870da94fb
AcePRbk[Rodney] <rodney@smsgh.com>
2016-05-27 20:20:45 +0000

    Added ui comoponents for HomeActivity

--- app/src/main/java/com/swiftpot/android/teletrosky/HomeActivity.java
+++ app/src/main/java/com/swiftpot/android/teletrosky/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.swiftpot.android.teletrosky;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.swiftpot.android.teletrosky.services.RegistrationIntentService;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    static final int REQUEST_CODE_RECOVER_PLAY_SERVICES = 1001;
+    int isAvailableCode ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +31,14 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +48,13 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        if (checkGoogleServicesPresenceAndAct()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
     }
 
     @Override
@@ -97,5 +112,51 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    public boolean checkGoogleServicesPresenceAndAct(){
+        boolean isServicePresent = false;
+        if(isServicesPresent()){
+            //do nothing and continue execution as its present already
+            isServicePresent = true;
+        }else{
+            if (GoogleApiAvailability.getInstance().isUserResolvableError(isAvailableCode)) {
+                showErrorDialog(isAvailableCode);
+
+            } else {
+                Toast.makeText(this, "This device is not supported.Install Google Play Services", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            //isServicePresent = false; //unneccessary since isServicePresent is already false
+        }
+
+        return isServicePresent;
+    }
+
+    public void showErrorDialog(int errorCode){
+        GoogleApiAvailability.getInstance().getErrorDialog(this,errorCode,REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
+    }
+    public boolean isServicesPresent(){
+        boolean serviceStatus = false;
+
+        isAvailableCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        if(isAvailableCode == ConnectionResult.SUCCESS){
+            serviceStatus = true;
+        }
+        else{
+            //serviceStatus is already false
+        }
+
+        return serviceStatus;
     }
 }
